a998d20b0eda4988fd416dcb696e6fad18db3003
J. Simon Richard <jsimonrichard@gmail.com>
2024-03-29 12:28:25 -0400

    Add getGroupMembers and arithmetic functions

--- group.java
+++ group.java
@@ -33,4 +33,40 @@ public class GitAssignment_group2 {
         System.out.println("Simon Richard / jsimonrichard");
     }
     
+    public String[] getGroupMembers() {
+    	return new String[] {
+    			"Simon Richard",
+    	};
+    }
+    
+    public int divide(int num1, int num2) {
+    	if(num2 == 0) {
+    		throw ArithmeticException("div by zero");
+    	}
+    	
+    	int quotient = 0;
+    	while(num1 > num2) {
+    		num1 -= num2;
+    		quotient++;
+    	}
+    	
+    	return quotient;
+    }
+    
+    public int multiply(int num1, int num2) {
+    	int product = 0;
+    	for(int i = 0; i < num1; i++) {
+    		product += num2;
+    	}
+    	return product;
+    }
+    
+    public int power(int num1, int num2) {
+    	int p = 1;
+    	for(int i = 0; i < num2; i++) {
+    		p = multiply(p, num1);
+    	}
+    	return p;
+    }
+    
 }
