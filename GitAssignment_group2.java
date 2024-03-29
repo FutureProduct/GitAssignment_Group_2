e39cffa496508a131ff3618f625efdac00471124
FutureProduct <162730806+FutureProduct@users.noreply.github.com>
2024-03-29 11:38:39 -0400

    Add files via upload
    Base Java file for group.java

---
+++ GitAssignment_group2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitassignment_group2;
+
+/**
+ *
+ * @author gamer
+ */
+public class GitAssignment_group2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
