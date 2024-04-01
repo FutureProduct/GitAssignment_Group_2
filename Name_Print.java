91f430a693cab3bc53c04567ef2f3adb2101942a
ErnakSalmander <163885011+ErnakSalmander@users.noreply.github.com>
2024-04-01 01:54:25 -0400

    Add files via upload

---
+++ Name_Print.java
@@ -0,0 +1,13 @@
+
+public class Name_Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void Ruslan() {
+		
+		System.out.println("Ruslan Averin / ErnakSalmander");
+	}
+}
